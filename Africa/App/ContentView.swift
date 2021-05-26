1564cf52bf01ee73afbf833bf16b7f5fe13b2e47
All3yp <noyalle@ualberta.ca>
2021-05-26 18:22:13 -0300

    Create decode bundle json and set coverImageView to contentView

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Content View 🦁")
+        // MARK: - PROPERTIES
+
+        // MARK: - BODY
+        NavigationView {
+            List {
+                CoverImageView()
+                    .frame(height: 300)
+                    .listRowInsets(
+                        EdgeInsets(
+                            top: 0,
+                            leading: 0,
+                            bottom: 0,
+                            trailing: 0)
+                    )
+            } //: List
+            .navigationBarTitle("Africa", displayMode: .large)
+        } //: Nav
             .padding()
     }
 }
 
+
+
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+// MARK: - PROPERTIES
+
+// MARK: - BODY
+
+// MARK: - PREVIEW
---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Alley Pereira on 26/05/21.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
