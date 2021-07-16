9300b293e7f8a7b976e167bd870ed3202ffbbffe
All3yp <noyalle@ualberta.ca>
2021-07-16 17:51:59 -0300

    Create LazyVGrid to show animal images on screen, create slider to change image if is swipped

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,14 +8,68 @@
 import SwiftUI
 
 struct GalleryView: View {
+
+    // MARK: - PROPERTIES
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+
+    @State private var selectedAnimal: String = "lion"
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+
+    // let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+
+    // MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 30) {
+
+                // MARK: - Selected Image
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+
+                // MARK: - Slider
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, perform: { value in
+                        gridSwitch()
+                    })
+
+                // MARK: - Grid
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    } //: ForEach
+                } //: LazyVGrid
+                .animation(.easeIn)
+                .onAppear(perform: {
+                    gridSwitch()
+                }) //: onAppear
+            } //: VStack
+
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         } //: Scroll
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
     }
 }
 
+// MARK: - PREVIEW
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
         GalleryView()
